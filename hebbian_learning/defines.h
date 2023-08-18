37604ae9b6532dcdbac2af21602aecb0711deac0
LucasHJesus <lukaslima641@gmail.com>
2023-08-17 21:47:27 -0300

    projeto 1

---
+++ hebbian_learning/defines.h
@@ -0,0 +1,9 @@
+#define     INPUT           [[-1,-1],\
+                             [-1, 1],\
+                             [ 1,-1],\
+                             [ 1, 1]]
+
+#define     TARGET_AND      [-1,-1,-1, 1]           
+#define     TARGET_OR       [-1, 1, 1, 1]
+
+#define     ABBA            10
