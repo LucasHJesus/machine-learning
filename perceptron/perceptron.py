e7dbdd18bc6407ba28dd3aafba3d4b6c4db3c565
LucasHJesus <lukaslima641@gmail.com>
2023-08-22 18:49:12 -0300

    manutenção  basica

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -10,15 +10,23 @@ class perceptron:
     def __error__() -> float:
         pass
 
-    def __init__(self, inputSize:int) -> None:
+    def __init__(self, inputSize:int, alph:float) -> None:
         self.weights = np.array([random.random()-0.5 for _ in range(inputSize)])
+        self.alpha = alph
         self.maxEpoch = 100
         pass
 
     def train(self, dataset:list, target:int) -> bool:
-
+        dataset = self.__linearize__(dataset)
         data = np.array(dataset)
         for i in range(self.maxEpoch):
             for j in range(len(data.size)):
                 np.dot(data,self.weights)
                 
+    def __linearize__(matrix:list) -> list:
+        output:list
+        output = []
+        for line in matrix:
+            for item in line:
+                output.append(item)
+        return output
